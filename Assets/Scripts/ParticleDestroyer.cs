aef5af7a6f62afeefd0edd089a211bf0f11c0632
Matt Blickem <matt@supercore.co.uk>
2016-08-27 20:06:15 +0100

    Add bullet effects

---
+++ Assets/Scripts/ParticleDestroyer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleDestroyer:MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(Lifetime());
+    }
+
+    IEnumerator Lifetime() {
+        float lifetime = 0.0f;
+        var particles = GetComponent<ParticleSystem>();
+        if (particles) {
+            lifetime = particles.duration + particles.startLifetime;
+        }
+        var sound = GetComponent<AudioClip>();
+        if (sound) {
+            lifetime = Mathf.Max(sound.length, lifetime);
+        }
+        yield return new WaitForSeconds(lifetime + 0.1f);
+        Destroy(gameObject);
+    }
+
+}
--- Assets/SimpleMultiplayerExample/Bullet.cs
+++ Assets/SimpleMultiplayerExample/Bullet.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
+    public GameObject impactEffectPrefab;
+
 	void OnCollisionEnter(Collision collision) {
         var hit = collision.gameObject;
         var health = hit.GetComponent<Health>();
         if (health) {
             health.TakeDamage(10);
+            Instantiate(impactEffectPrefab, collision.contacts[0].point, hit.transform.rotation);
         }
         Destroy(gameObject);
     }
