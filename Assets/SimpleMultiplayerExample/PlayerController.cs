defa47373479f004ca81c0a218f56f1315c972c9
Matt Blickem <matt@supercore.co.uk>
2016-08-27 19:40:47 +0100

    Fix player movement and camera

--- Assets/SimpleMultiplayerExample/PlayerController.cs
+++ Assets/SimpleMultiplayerExample/PlayerController.cs
@@ -5,46 +5,51 @@ using UnityEngine.Networking;
 public class PlayerController : NetworkBehaviour {
 
    
-    public float xSpeed = 2.0f;
-    public float zSpeed = 2.0f;
-	public Camera playerCam;
-
+    public float moveSpeed = 2.0f;
+    public float rotateSpeed = 5.0f;
 
     private Transform body;
-    private Transform gun;
+    private Transform head;
+    private GameObject weapon;
 	private float rotationY = 0.0f;
 	private float rotationX = 0.0f;
+    private Vector3 movementCommand;
 
-
-    public override void OnStartLocalPlayer() {
-        body = transform.FindChild("Body");
-        body.GetComponent<MeshRenderer>().material.color = Color.blue;
-    }
 
     void Start() {
-        gun = transform.FindChild("Gun");
+        head = transform.FindChild("Head");
+        // TODO(MDB): Make weapons droppable and equippable
+        weapon = head.transform.FindChild("Gun").gameObject;
 		Cursor.visible = false;
+        if (isLocalPlayer) {
+            body = transform.FindChild("Body");
+            body.GetComponent<MeshRenderer>().material.color = Color.blue;
+            head.FindChild("Camera").gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
     void Update () {
         if (!isLocalPlayer) return; // only update on local client (object owner)
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * xSpeed;
-        var z = Input.GetAxis("Vertical") *  Time.deltaTime * zSpeed;
-        transform.Translate(x, 0, 0);
-        transform.Translate(0, 0, z);
-		rotationX += Input.GetAxis ("Mouse X") * 5.0f;
-		rotationY += Input.GetAxis ("Mouse Y") * 5.0f;
-		transform.eulerAngles = new Vector3 (-rotationY, rotationX, 0.0f);
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime;
+        var z = Input.GetAxis("Vertical") *  Time.deltaTime;
+        movementCommand = (transform.forward * z + transform.right * x).normalized * moveSpeed;
+		rotationX += Input.GetAxis ("Mouse X") * rotateSpeed;
+		rotationY += Input.GetAxis ("Mouse Y") * rotateSpeed;
+        transform.eulerAngles = new Vector3 (-rotationY, rotationX, 0.0f);
         if(Input.GetButton("Fire1")) {
             CmdFire();
         }
 	}
 
+    void FixedUpdate() {
+        GetComponent<Rigidbody>().AddForce(movementCommand);
+    }
+
     // Execute on server
     [Command]
     void CmdFire() {
-        gun.SendMessage("Fire");
+        weapon.SendMessage("Fire");
     }
 }
