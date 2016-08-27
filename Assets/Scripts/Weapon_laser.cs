771e698cfd833a058dd2af6025e75e4a2cc6b484
Daniel Cohen <analoguecolour@gmail.com>
2016-08-27 23:34:35 +0100

    laser weapon work (line renderer)

--- Assets/Scripts/Weapon_laser.cs
+++ Assets/Scripts/Weapon_laser.cs
@@ -6,10 +6,14 @@ public class Weapon_laser : MonoBehaviour {
 	public float laserCooldown = 1.0f;
 	private Transform nozzle;
 	private bool cooldown;
+	public LineRenderer LaserLR;
 
 	void Start() {
 		nozzle = transform.FindChild("Nozzle");
 		cooldown = false;
+		LaserLR.SetVertexCount(2);
+		LaserLR.SetWidth(0.5f, 0.5f);
+		LaserLR.enabled = false;
 	}
 
 	IEnumerator Fire() {
@@ -19,7 +23,12 @@ public class Weapon_laser : MonoBehaviour {
 		cooldown = true;
 		Vector3 fwd = nozzle.TransformDirection(Vector3.forward);
 		RaycastHit rHit;
-		Debug.DrawRay (nozzle.position, fwd * 100.0f, Color.magenta, 2.0f);
+		//Debug.DrawRay (nozzle.position, fwd * 100.0f, Color.magenta, 2.0f);
+
+		LaserLR.enabled = true;
+		LaserLR.SetPosition(0, nozzle.position);
+		LaserLR.SetPosition(1, fwd * 100.0f);
+
 		if (Physics.Raycast (nozzle.position, fwd, out rHit, 100.0f)) {
 				var hit = rHit.transform.gameObject;
 				var health = hit.GetComponent<Health>();
