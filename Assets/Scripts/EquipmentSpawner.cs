d10fa752293d2b97757290b6f26f90f30352176a
Matt Blickem <matt@supercore.co.uk>
2016-08-29 12:36:08 +0100

    Fix equipment spawner never spawning lasers

--- Assets/Scripts/EquipmentSpawner.cs
+++ Assets/Scripts/EquipmentSpawner.cs
@@ -7,13 +7,15 @@ public class EquipmentSpawner:NetworkBehaviour {
     public GameObject[] equipment;
 
     public override void OnStartServer() {
-        var index = Random.Range(0, equipment.Length-1);
+        var index = Random.Range(0, equipment.Length);
         var item = (GameObject)Instantiate(equipment[index], transform.position, transform.rotation);
         NetworkServer.Spawn(item);
     }
 
-
     void OnDrawGizmos() {
-        Gizmos.DrawCube(transform.position, Vector3.one);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position, Vector3.one);
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, Vector3.one * 0.5f);
     }
 }
