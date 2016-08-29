df50b711168172823933588a625ffa6fc7d36f34
Daniel Cohen <analoguecolour@gmail.com>
2016-08-29 13:41:53 +0100

    start asteroid prefab

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+	public float xSpin;
+	public float ySpin;
+	public float zSpin;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xSpin = Random.Range (10, 50);
+		ySpin = Random.Range (10, 50);
+		zSpin = Random.Range (10, 50);
+		transform.Rotate (xSpin, ySpin, zSpin);
+	}
+}
